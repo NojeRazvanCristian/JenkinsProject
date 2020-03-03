3d927faf90f9ae2fe42f14cf651e17777ea30898
Noje Razvan <>
2020-03-03 12:37:10 +0200

    batch

---
+++ batchfile.bat
@@ -0,0 +1,5 @@
+set projectLocation=Apple\Users\nojerazvan\Desktop\JenkinsProject
+cd Apple\Users\nojerazvan\Desktop\JenkinsProject
+set classpath=‎Apple\Users\nojerazvan\Desktop\JenkinsProject\bin;Apple\Users\nojerazvan\Desktop\JenkinsProject\lib\*
+java org.testng.TestNG Apple\Users\nojerazvan\Desktop\JenkinsProject\testng.xml
+pause
